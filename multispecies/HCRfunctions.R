09a82924046ba0fa9978c2a4ca13d077d2cd06a6
Sam Schiano <85180369+seschiano@users.noreply.github.com>
2022-01-13 11:39:38 -0500

    Update HCRfunctions.R

--- multispecies/HCRfunctions.R
+++ multispecies/HCRfunctions.R
@@ -5,6 +5,22 @@ control <-  function() {
   
 }
 
+#control <- function(estimated.biomass, control.pars) {
+#  H1 <- control.pars$H1
+#  H2 <- control.pars$H2
+#  Bmax <- control.pars$Bmax
+#  B2 <- control.pars$B2
+#  B1 <- control.pars$B1
+  
+#  harv <- ifelse(estimated.biomass >= B1, H1,
+#                 ifelse(estimated.biomass < B2, H2,
+#                        (H1-H2)/(B1-B2)*(estimated.biomass - B2) + H2))
+  
+#  return(harv)
+  
+#end function control
+#}
+
 #Plateau-slope-plateau-slop HCR  
   control <- function(estimated.biomass, control.pars) {
     H1 <- control.pars$H1
